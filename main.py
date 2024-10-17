8c0b6045ef648e19107f2a6a12de6bac63365dc4
HP <fabioford707@gmail.com>
2024-10-17 13:01:10 -0400

    Practice Inform

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import pandas as pd
-import numpy as np
 from abc import ABC, abstractmethod
+import sqlite3
 
 
 class Document(ABC):
@@ -12,42 +12,109 @@ class Document(ABC):
     def readDoc(self):
         pass
 
-    @abstractmethod
-    def writeDoc(self, out):
-        pass
-
 
 class CSV(Document):
     def __init__(self, filepath):
         super().__init__(filepath)
-        self.document = None
 
     def readDoc(self):
-        self.document = pd.read_csv(self.filepath)
-        return self.document
+        return pd.read_csv(self.filepath)
 
-    def writeDoc(self, out):
-        if self.document is not None:
-            return self.document.to_csv(out)
+
+class Excel(Document):
+    def __init__(self, filepath):
+        super().__init__(filepath)
+
+    def readDoc(self):
+        return pd.read_excel(self.filepath)
+
+
+class JSON(Document):
+    def __init__(self, filepath):
+        super().__init__(filepath)
+
+    def readDoc(self):
+        return pd.read_json(self.filepath)
+
+
+class Parquet(Document):
+    def __init__(self, filepath):
+        super().__init__(filepath)
+
+    def readDoc(self):
+        return pd.read_parquet(self.filepath)
+
+
+class HDF(Document):
+    def __init__(self, filepath):
+        super().__init__(filepath)
+
+    def readDoc(self):
+        return pd.read_hdf(self.filepath)
+
+
+class Feather(Document):
+    def __init__(self, filepath):
+        super().__init__(filepath)
+
+    def readDoc(self):
+        return pd.read_feather(self.filepath)
+
+
+class Stata(Document):
+    def __init__(self, filepath):
+        super().__init__(filepath)
+
+    def readDoc(self):
+        return pd.read_stata(self.filepath)
+
+
+class SAS(Document):
+    def __init__(self, filepath):
+        super().__init__(filepath)
+
+    def readDoc(self):
+        return pd.read_sas(self.filepath)
+
+
+class SPSS(Document):
+    def __init__(self, filepath):
+        super().__init__(filepath)
+
+    def readDoc(self):
+        return pd.read_spss(self.filepath)
+
+
+class SQL(Document):
+    def __init__(self, filepath):
+        super().__init__(filepath)
+
+    def readDoc(self):
+        conn = sqlite3.connect(self.filepath)
+        query = "SELECT * FROM your_table"  # Hace falta modificar segun la consulta
+        return pd.read_sql_query(query, conn)
+
+
+class Pickle(Document):
+    def __init__(self, filepath):
+        super().__init__(filepath)
+
+    def readDoc(self):
+        return pd.read_pickle(self.filepath)
 
 
 class CreateDocumentObject(ABC):
     @abstractmethod
     def __init__(self, filepath):
         self.filepath = filepath
-        self.document = None
 
     @abstractmethod
     def createDocument(self):
         pass
 
     def readDocument(self):
-        self.document = self.createDocument()
-        return self.document.readDoc()
-
-    def writeDocument(self, out):
-        if self.document is not None:
-            return self.document.writeDoc(out)
+        document = self.createDocument()
+        return document.readDoc()
 
 
 class CreateCSV(CreateDocumentObject):
@@ -56,6 +123,86 @@ class CreateCSV(CreateDocumentObject):
 
     def createDocument(self):
         return CSV(self.filepath)
+
+
+class CreateExcel(CreateDocumentObject):
+    def __init__(self, filepath):
+        super().__init__(filepath)
+
+    def createDocument(self):
+        return Excel(self.filepath)
+
+
+class CreateJSON(CreateDocumentObject):
+    def __init__(self, filepath):
+        super().__init__(filepath)
+
+    def createDocument(self):
+        return JSON(self.filepath)
+
+
+class CreateParquet(CreateDocumentObject):
+    def __init__(self, filepath):
+        super().__init__(filepath)
+
+    def createDocument(self):
+        return Parquet(self.filepath)
+
+
+class CreateHDF(CreateDocumentObject):
+    def __init__(self, filepath):
+        super().__init__(filepath)
+
+    def createDocument(self):
+        return HDF(self.filepath)
+
+
+class CreateFeather(CreateDocumentObject):
+    def __init__(self, filepath):
+        super().__init__(filepath)
+
+    def createDocument(self):
+        return Feather(self.filepath)
+
+
+class CreateStata(CreateDocumentObject):
+    def __init__(self, filepath):
+        super().__init__(filepath)
+
+    def createDocument(self):
+        return Stata(self.filepath)
+
+
+class CreateSAS(CreateDocumentObject):
+    def __init__(self, filepath):
+        super().__init__(filepath)
+
+    def createDocument(self):
+        return SAS(self.filepath)
+
+
+class CreateSPSS(CreateDocumentObject):
+    def __init__(self, filepath):
+        super().__init__(filepath)
+
+    def createDocument(self):
+        return SPSS(self.filepath)
+
+
+class CreateSQL(CreateDocumentObject):
+    def __init__(self, filepath):
+        super().__init__(filepath)
+
+    def createDocument(self):
+        return SQL(self.filepath)
+
+
+class CreatePickle(CreateDocumentObject):
+    def __init__(self, filepath):
+        super().__init__(filepath)
+
+    def createDocument(self):
+        return Pickle(self.filepath)
 
 
 class DocumentFactory:
@@ -75,8 +222,21 @@ class DocumentFactory:
 
 factory = DocumentFactory()
 factory.registerFormat('csv', CreateCSV)
+factory.registerFormat('xlsx', CreateExcel)
+factory.registerFormat('json', CreateJSON)
+factory.registerFormat('parquet', CreateParquet)
+factory.registerFormat('h5', CreateHDF)
+factory.registerFormat('feather', CreateFeather)
+factory.registerFormat('dta', CreateStata)
+factory.registerFormat('sas7bdat', CreateSAS)
+factory.registerFormat('sav', CreateSPSS)
+factory.registerFormat('sqlite', CreateSQL)
+factory.registerFormat('pkl', CreatePickle)
 
-doc = factory.getDocument("data/raw/iris.csv")
-
+# Ejemplo de uso
+doc = factory.getDocument("data/raw/Jsonaexcel.xlsx")
 df = doc.readDocument()
+print(df)
+df = df.drop("Column1.created_date", axis=1)
+df = df.drop("Column1.id", axis=1)
 print(df)
